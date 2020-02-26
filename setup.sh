82a8efd09cdb9a2d26488a1e5249cc657238dbdf
soonraah <harasono@fout.jp>
2020-02-26 20:50:07 +0900

    Fix setup.sh not only for .z* files

--- setup.sh
+++ setup.sh
@@ -7,7 +7,12 @@ if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
     ln -s ~/dotfiles/prezto ~/.zprezto
 fi
 
-for file in .z*; do
-    rm -i ~/$file
+# copy dot fliles to ~
+for file in .*; do
+    if [[ $file =~ ^\.git || $file =~ ^\.\.?$ ]]; then
+        continue
+    fi
+    echo "Making linke of $file"
+    rm -f -i ~/$file
     ln -s ~/dotfiles/$file ~/$file
 done
