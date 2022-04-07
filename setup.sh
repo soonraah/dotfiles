9c642e91d62c1dc576cfd7fe722c6dbe832332a6
soonraah <soonraah.dev@gmail.com>
2022-04-08 01:31:52 +0900

    Update setup script for ZI and Starship

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 
 #!/bin/sh
 
+set -eu
+
 cd ~/dotfiles
 
 if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
@@ -19,6 +21,24 @@ done
 
 # for tmux copy mode
 if [ `uname` = "Darwin" ]; then
+    brew update
+
     echo 'This is MacOS. Installing reattach-to-user-namespace by homebrew'
     brew install reattach-to-user-namespace
+
+    brew install starship
+    brew tap homebrew/cask-fonts
+    brew install font-jetbrains-mono-nerd-font
+else
+    curl -sS https://starship.rs/install.sh | sh
 fi
+
+# install ZI
+sh -c "$(curl -fsSL https://git.io/get-zi)" --
+
+# config file for Starship
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+rm -f -i ~/.config/starship.toml
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
